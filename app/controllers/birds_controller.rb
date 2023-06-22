63af2dbbb3b896f8c94b25dc99f5cae52c562e39
Serah-Njenga <serah35.nn@gmail.com>
2023-06-22 11:20:24 +0300

    commit

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,10 @@
 class BirdsController < ApplicationController
 
+  # POST /birds
+  def create
+    byebug
+  end
+
   # GET /birds
   def index
     birds = Bird.all
@@ -14,6 +19,19 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+
+    def create
+      bird = Bird.create(name: params[:name], species: params[:species])
+      render json: bird, status: :created
+    end
+  
+    # GET /birds/:id
+# def show
+#   # params[:id] refers to the dynamic part of our route, defined by :id
+#   # a request to /birds/2 would give params[:id] a value of 2
+#   bird = Bird.find_by(id: params[:id])
+#   render json: bird
+# end
   end
 
 end
